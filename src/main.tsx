60497e47241bfd23c48b06ef7645a9011bbe1952
천성윤 <1112csy@naver.com>
2025-03-05 17:46:46 +0900

    🎉: 프로젝트 초기 설정

---
+++ src/main.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import App from "./App";
+import "@/index.css";
+
+const queryClient = new QueryClient();
+
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </React.StrictMode>
+  );
+} else {
+  console.error("Root element not found!");
+}
