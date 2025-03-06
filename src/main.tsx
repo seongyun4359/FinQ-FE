daf788aa1f802f3c19bf503a02714bd8b8709f93
천성윤 <1112csy@naver.com>
2025-03-06 13:21:31 +0900

    ㅇ

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App";
 import "./index.css";
 
